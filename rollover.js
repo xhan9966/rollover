879a4820a3826a1fb5acaa06ab2449b0f7d31838
Tony <xhan9966@conestogac.on.ca>
2022-06-13 16:10:23 -0400

    preload images and setTimeout task.

--- rollover.js
+++ rollover.js
@@ -1,21 +1,32 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll("#image_rollovers img");
+  const images = document.querySelectorAll("#image_rollovers img");
 
-    // process each img tag
-    for (let image of images) {
-        const oldURL = image.src;
-        const newURL = image.id;
+  // process each img tag
+  for (let image of images) {
+    const oldURL = image.src;
+    const newURL = image.id;
 
-        // preload rollover image
+    // preload rollover image
+    const imageEle = new Image();
+    imageEle.src = newURL;
 
-        // set up event handlers for hovering an image
-        image.addEventListener("mouseover", () => {
-            image.src = newURL;
-        });
-        image.addEventListener("mouseout", () => {
-            image.src = oldURL;
-        });
-    }
+    // set up event handlers for hovering an image
+    image.addEventListener("mouseover", () => {
+      image.src = newURL;
+    });
+    image.addEventListener("mouseout", () => {
+      image.src = oldURL;
+    });
+
+    // setup timer.
+    setTimeout(() => {
+      image.src = newURL;
+    }, 1000);
+
+    setTimeout(() => {
+      image.src = oldURL;
+    }, 2000);
+  }
 });
